10ec15ad0fa18767e6da798b1c44adde4b59bd47
wangyong <ywang_perfect@163.com>
2023-02-12 10:36:18 +0800

    feat: init db ORM

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@
 """
 from flask import Flask
 from app.config import setting
+from app.models import db
 
 
 def create_app() -> Flask:
@@ -11,6 +12,7 @@ def create_app() -> Flask:
 
     app.config.from_object(setting)
     register_blueprints(app)
+    configure_db(app)
 
     return app
 
@@ -19,3 +21,10 @@ def register_blueprints(app: Flask):
     from app.api.v1 import create_v1
 
     app.register_blueprint(create_v1(), url_prefix="/api/v1")
+
+
+def configure_db(app: Flask):
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
