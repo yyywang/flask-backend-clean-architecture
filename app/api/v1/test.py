c2bea81ab4e711f0ff808cc1fb736c6a6fdd3746
wangyong <ywang_perfect@163.com>
2023-02-12 10:36:56 +0800

    test: added get & post user test

--- app/api/v1/test.py
+++ app/api/v1/test.py
@@ -2,6 +2,8 @@
 """
   Created by 怀月 on 2023/1/12.
 """
+from app.models import db, User
+from app.utils.fake import fake
 from app.utils.redprint import Redprint
 
 test_api = Redprint('test')
@@ -10,3 +12,19 @@ test_api = Redprint('test')
 @test_api.route('')
 def hello():
     return 'test ok'
+
+
+@test_api.route('/db/user', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return [user.nickname for user in users]
+
+
+@test_api.route('/db/user', methods=['POST'])
+def create_user():
+    nickname = fake.name()
+    with db.auto_commit():
+        user = User(nickname=nickname)
+        db.session.add(user)
+
+    return 'created user: %s' % nickname
